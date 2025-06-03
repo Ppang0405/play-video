ae7ae23a4c2308e6ec33a0daa681687dcfc70bd0
truongnguyen3 <truong.nguyen3@niteco.se>
2025-06-03 16:57:12 +0700

    Initial Commit

---
+++ video-player/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  video-player
+//
+//  Created by Truong Huu Nguyen on 3/6/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ video-player/video_playerApp.swift
@@ -0,0 +1,17 @@
+//
+//  video_playerApp.swift
+//  video-player
+//
+//  Created by Truong Huu Nguyen on 3/6/25.
+//
+
+import SwiftUI
+
+@main
+struct video_playerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
